000b716e590a6e2f108190f257d5f1c3544516e1
Wm Salt Hale <salt@altsalt.net>
2021-11-09 13:22:32 -0800

    removed stickerpicker subfolder, forked as separate repo

--- stickerpicker/sticker/version.py
+++
@@ -1,6 +0,0 @@
-# Generated in setup.py
-
-git_tag = None
-git_revision = '6ad3bf2f'
-version = '0.1.0+dev.6ad3bf2f'
-linkified_version = '0.1.0+dev.[6ad3bf2f](https://github.com/maunium/stickerpicker/commit/6ad3bf2f5b7a3cb84d7d432e10175d7ed42259d0)'
